882e7de99fffe9576fc84748ee68c97a9d4bbf74
Mathijs455 <73833862+Mathijs455@users.noreply.github.com>
2021-09-29 09:33:58 +0200

    Initial create vue

---
+++ Front-end/customerapp-vue/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable type="dark" variant="dark">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="navbar-toggle-collapse">
+        <template #default="{ expanded }">
+          <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+          <b-icon v-else icon="chevron-bar-down"></b-icon>
+        </template>
+      </b-navbar-toggle>
+
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/">Home</b-nav-item>
+          <b-nav-item href="/about">About</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
