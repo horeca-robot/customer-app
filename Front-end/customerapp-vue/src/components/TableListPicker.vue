57207c9f1d40c825271f4f3a4416421cbe3ef3ef
KYPremco <KYPremco@gmail.com>
2021-10-13 14:32:06 +0200

    Implemented table list picker

---
+++ Front-end/customerapp-vue/src/components/TableListPicker.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-list-group>
+    <b-list-group-item
+      v-for="table in tables"
+      :key="table.id"
+      href="#"
+      @click="redirectToTable(table.id)"
+      >Tafel {{ table.number }}</b-list-group-item
+    >
+  </b-list-group>
+</template>
+
+<script>
+import router from "../router";
+
+export default {
+  name: "TableListPicker",
+  props: {
+    tables: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    redirectToTable(tableId) {
+      this.$store.dispatch("tableModule/setTable", tableId);
+      router.push("menu");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
