98d603284140d0d43694c7a1a1f5d128a0fc9658
Teun van Brakel <teunvbrakel@gmail.com>
2021-10-20 11:49:48 +0200

    Product page finished

--- Front-end/customerapp-vue/src/components/ProductCard.vue
+++ Front-end/customerapp-vue/src/components/ProductCard.vue
@@ -1,17 +1,6 @@
 <template>
-  <!-- <div>
-    <b-card v-b-modal="product.id.toString()" class="card" :img-src="product.img" img-top>
-      <b-card-text> € {{ product.price.toFixed(2) }} </b-card-text>
-    </b-card>
-    <b-modal :id="product.id.toString()">
-      <div style="margin-bottom: 20px" v-for="ingredient in product.ingredients" :key="ingredient.id">
-        <li>{{ingredient.name}}</li>
-      </div>
-      <b-button pill variant="success" @click="addToCart()">Add</b-button>
-    </b-modal>
-  </div> -->
   <div>
-    <b-card class="productCard">
+    <b-card class="productCard" v-on:click="GoToProduct(product.id)">
       <h1 class="productName">{{product.name}}</h1>
       <div class="productImage">
         <b-card-img :src="product.img" class="Image"></b-card-img>
@@ -31,6 +20,9 @@ export default {
       this.$store.commit("addToCart", this.product);
       this.$bvModal.hide(this.product.id.toString())	
     },
+    GoToProduct(product){
+        this.$router.push({name:'Product', params: {categoryId: this.$route.params.id, id: product}})
+    }
   },
 };
 </script>
