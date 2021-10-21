d3b07018727743d255da83774f1969dead0c6324
KingReload <w.arends@hotmail.com>
2021-10-21 15:35:15 +0200

    finished the productcard

--- Front-end/customerapp-vue/tests/unit/components/ProductCard.spec.js
+++ Front-end/customerapp-vue/tests/unit/components/ProductCard.spec.js
@@ -24,4 +24,24 @@ describe("ProductCard.vue", () => {
       await wrapper.trigger('click', { button: 0 });
       expect(wrapper.vm.$route).toBe("/menu/2/1");
   });
+
+  it("Test the addtocart method.", () => {
+        let product = { id: "a7f8fa32-4d59-4641-b9b5-33596d46aa6a", name: "Hamburger", price: 11.5, img: "https://www.sameneenkoekopen.nl/uploads/images/recept/hamburger-uit-de-pan.jpg" };
+        const $store =  {
+          commit: jest.fn(),
+        };
+        const $bvModal = {
+          hide: jest.fn(),
+        };
+        let wrapper = shallowMount(ProductCard, {
+            propsData: { product },
+            mocks: {
+                $store,
+                $bvModal
+            }
+        });
+        wrapper.vm.$store.commit("addToCart", wrapper.props().product);
+        wrapper.vm.$bvModal.hide(wrapper.props().product.id.toString());
+        expect(wrapper.vm.$store).toEqual($store);
+  });
 });
---
+++ Front-end/customerapp-vue/tests/unit/views/Cart.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from "@vue/test-utils";
+import Cart from "@/views/Cart.vue";
+
+describe("Cart.vue", () => {
+  it("Test gotomenu method.", () => {
+      
+  });
+});
