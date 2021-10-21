2a0d82e17c51b49760b282084122e8509dc2a55d
KingReload <w.arends@hotmail.com>
2021-10-21 13:58:57 +0200

    Byproduct unit tests finished

---
+++ Front-end/customerapp-vue/tests/unit/components/Byproduct.spec.js
@@ -0,0 +1,24 @@
+import { shallowMount } from "@vue/test-utils";
+import Byproduct from "@/components/Byproduct.vue";
+
+describe("Byproduct.vue", () => {
+  it("Test inserting a byproduct.", () => {
+    let byproduct = { name: "Tomaat" };
+    let wrapper = shallowMount(Byproduct, {
+      propsData: { byproduct },
+    });
+    expect(wrapper.text()).toMatch(byproduct.name);
+  });
+
+  it("Test amount changing.", async () => {
+    let byproduct = { name: "Tomaat" };
+    let wrapper = shallowMount(Byproduct, {
+      propsData: { byproduct },
+    });
+    expect(wrapper.vm.amount.toString()).toMatch("0");
+    wrapper.vm.AddToAmount();
+    expect(wrapper.vm.amount.toString()).toMatch("1");
+    wrapper.vm.RemoveFromAmount();
+    expect(wrapper.vm.amount.toString()).toMatch("0");
+  });
+});
