4c9b47a24b4db38f083cb4bf7dade22bd96386ee
KingReload <w.arends@hotmail.com>
2021-11-03 10:39:53 +0100

    cart unit tests done

--- Front-end/customerapp-vue/tests/unit/views/Cart.spec.js
+++ Front-end/customerapp-vue/tests/unit/views/Cart.spec.js
@@ -2,7 +2,37 @@ import { shallowMount } from "@vue/test-utils";
 import Cart from "@/views/Cart.vue";
 
 describe("Cart.vue", () => {
-  it("Test gotomenu method.", () => {
-      
+  it("Test gotomenu method.", async () => {
+    const $store = {
+      commit: jest.fn(),
+      getters: { cartItems: 0, cartTotal: 0 },
+    };
+
+    let wrapper = shallowMount(Cart, {
+      mocks: {
+          $route: "/menu",
+          $store,
+      }
+    });
+    await wrapper.trigger('click', { button: 2 });
+    expect(wrapper.vm.$route).toBe("/menu");
+  });
+
+  it("Test items and total in the local store.", () => {
+    const $store = {
+      commit: jest.fn(),
+      getters: { cartItems: 0, cartTotal: 0 },
+    };
+    
+    let wrapper = shallowMount(Cart, {
+      mocks: {
+        $store,
+      },
+    });
+
+    wrapper.vm.$store.commit("updateCartFromLocalStorage");
+    expect(wrapper.vm.$store).toEqual($store);
+    expect(wrapper.vm.$store.getters.cartItems).toEqual(0);
+    expect(wrapper.vm.$store.getters.cartTotal).toEqual(0);
   });
 });
