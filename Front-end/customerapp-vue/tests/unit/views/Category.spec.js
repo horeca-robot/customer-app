c1364107d079e0506dadb5508f2746ea1f48136f
Teun van Brakel <teunvbrakel@gmail.com>
2021-11-03 10:07:14 +0100

    Category page tests made

---
+++ Front-end/customerapp-vue/tests/unit/views/Category.spec.js
@@ -0,0 +1,32 @@
+import { mount, shallowMount } from "@vue/test-utils";
+import Categoryview from "@/views/Category.vue";
+
+describe("Category.vue", ()=>{
+    it("Test if correct amount of products are made", async() => {
+        var pizza = {id: 1, name: "Pizza", price: 12.34};
+        var coffee = {id: 3, name: "Coffee", price: 4.54};
+        var Products = [pizza, coffee];
+        var category = {name: "dessert", id: 1, img: "https://www.24kitchen.nl/files/styles/social_media_share/public/2020-08/verse-tomatensoep.jpg?itok=AqWg5Ju0" ,products: Products}
+        const axios =  {
+            get: ()=> Promise.resolve({data: category}), 
+        };
+        console.log(category);
+        const $store = {
+            commit: jest.fn(),
+        }
+        const $route = {
+            params: {
+                id: 10,
+            }
+        }
+        var wrapper = shallowMount(Categoryview, {
+            mocks: {
+            axios,
+            $store,
+            $route,
+            }
+        });
+        await wrapper.setData({category: category});
+        expect(wrapper.vm.category.products).toEqual(Products)
+    });
+})
