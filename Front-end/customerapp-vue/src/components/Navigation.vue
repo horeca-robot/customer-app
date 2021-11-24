bb5735d873f5563f8ebda4cdef5076699b95444a
Jurgenks <79445768+Jurgenks@users.noreply.github.com>
2021-11-24 13:34:01 +0100

    Update Navigation.vue

--- Front-end/customerapp-vue/src/components/Navigation.vue
+++ Front-end/customerapp-vue/src/components/Navigation.vue
@@ -65,7 +65,7 @@ export default {
         this.hide();
       },
       GoToCart() {
-                this.$router.push({name:'Cart'});
+                this.$router.push({name:'Winkelwagen'});
         this.hide();
       },
       GoToYourOrders() {
