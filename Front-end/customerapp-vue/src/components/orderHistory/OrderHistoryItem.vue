b97b0d6a7cf9f75335123a959f9b1a7fa7242c10
Mathijs455 <73833862+Mathijs455@users.noreply.github.com>
2021-12-01 15:51:17 +0100

    More details in order-history + order status

--- Front-end/customerapp-vue/src/components/orderHistory/OrderHistoryItem.vue
+++ Front-end/customerapp-vue/src/components/orderHistory/OrderHistoryItem.vue
@@ -1,21 +1,35 @@
 <template>
- <div>
-  <b-row
-    class="border-bottom border-secondary border-large pt-3"
-    @click="goToOrderDetail(order.id)"
-  >
-    <b-col>Bestelling #{{number}}</b-col>
-    
-
-    <b-col>
-      <b-row class="justify-content-end" >
-        <b-col class="text-right">
-          <span class="align-top me-2">€ {{ order.subTotal.toFixed(2) }}</span>  <span class="h3"><b-icon-eye-fill /> </span>
-        </b-col>
-      </b-row>
-    </b-col>
-  </b-row>
- </div>
+  <div class="container OrderHistoryItem">
+    <b-row
+      class="border-bottom border-secondary border-large pt-3"
+      @click="$root.$emit('bv::toggle::collapse', 'order-details' + number)"
+    >
+      <b-col>Bestelling #{{ number }}</b-col>
+      <b-col>
+        <b-row class="justify-content-end">
+          <b-col class="text-right">
+            <span class="align-top me-2">
+               € {{ order.subTotal.toFixed(2) }}
+               <b-icon v-if="order.paid" icon="check" variant="success"></b-icon>
+              <b-icon v-if="!order.paid" icon="x" variant="danger"></b-icon>
+               </span>
+            <span class="h3"><b-icon icon="arrow-down-square-fill" font-scale="0.8"/> </span>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+    <b-collapse :id="'order-details' + number">
+      <b-container class="order-details text-start">
+        <b-row>
+          <p>Geplaatst op: {{checkDateDigits(new Date(order.createdAt).getHours())}}:{{checkDateDigits(new Date(order.createdAt).getMinutes())}}</p>
+          <p v-if="order.paid">Betaling: U heeft betaald</p>
+          <p v-if="!order.paid">Betaling: Nog niet betaald</p>
+          <p>{{CountDeliveredProducts(order)}} / {{ order.productOrders.length }} producten afgeleverd</p>
+          <b-button @click="goToOrderDetail(order.id)" class="button-style">Bekijk producten</b-button>
+        </b-row>
+      </b-container>
+    </b-collapse>
+  </div>
 </template>
 
 <script>
@@ -29,16 +43,49 @@ export default {
     goToOrderDetail(id) {
       this.$router.push({ name: "OrderHistoryDetails", params: { id } });
     },
+    CountDeliveredProducts(order){
+      var count = 0;
+      order.productOrders.forEach(prodorder => {
+        if(prodorder.orderStatus === "DELIVERED"){
+          count++
+        }
+      });
+      return count;
+    },
+    checkDateDigits(date){
+      if(date === 0){
+        return "00";
+      }
+      if(date < 10){
+        return "0" + date;
+      }
+      return date;
+    }
   },
 };
 </script>
 
 <style scoped>
+.OrderHistoryItem {
+  background-color: #e0dccc;
+
+}
+.order-details {
+  background-color: #bdad89;
+  padding: 20px;
+}
 .text-right {
   text-align: right;
 }
 .horizontal-center-text {
   margin: auto;
+}
+
+.button-style {
+  background-color: #e0dccc !important;
+  margin-bottom: 10px;
+  max-width: 40vw;
+  margin-left: 50vw;
 }
 
 .border-large {
