f8ea283a6c553c5c7d2d68ed1c811d57bc3f21d4
KYPremco <KYPremco@gmail.com>
2021-12-08 15:18:49 +0100

    Revert "Added tag service"
    This reverts commit dcfea4339ea08d1953d9578bdb127ce938bede4d.

--- Back-end/CustomerApp-Api/src/main/java/com/customerapp/CustomerAppApi/core/services/ProductService.java
+++ Back-end/CustomerApp-Api/src/main/java/com/customerapp/CustomerAppApi/core/services/ProductService.java
@@ -2,10 +2,7 @@ package com.customerapp.CustomerAppApi.core.services;
 
 import com.customerapp.CustomerAppApi.core.interfaces.IProductService;
 import edu.fontys.horecarobot.databaselibrary.models.Product;
-import edu.fontys.horecarobot.databaselibrary.models.Tag;
 import edu.fontys.horecarobot.databaselibrary.repositories.ProductRepository;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,27 +18,7 @@ public class ProductService implements IProductService {
     }
 
     public List<Product> getAllProducts() {
-        Product product = new Product();
-        List<Tag> tagList = new ArrayList<Tag>();
-        Tag tag = new Tag();
-        tag.setName("Alcohol");
-        tagList.add(tag);
-        product.setTags(tagList);
-
-        Example<Product> example = Example.of(product);
-        return productRepository.findAll(example);
-
-//        return productRepository.findAll();
-    }
-
-    public List<Product> getAllProducts(List<Integer> tagIds) {
-        Product product = new Product();
-        List<Tag> tagList = new ArrayList<Tag>();
-        tagList.add(new Tag(null, "DONKERS"));
-        product.setTags(tagList);
-
-        Example<Product> example = Example.of(product);
-        return productRepository.findAll(example);
+        return productRepository.findAll();
     }
 
     public Product getProductById(UUID id) {
