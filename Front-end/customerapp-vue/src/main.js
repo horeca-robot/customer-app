5069627de1fcf2303f65b4e5fe223b8d1ec33d6d
KingReload <w.arends@hotmail.com>
2021-12-09 09:14:54 +0100

    Update main.js

--- Front-end/customerapp-vue/src/main.js
+++ Front-end/customerapp-vue/src/main.js
@@ -9,7 +9,6 @@ import APIService from './services/axios.service';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(VueQrcodeReader);
